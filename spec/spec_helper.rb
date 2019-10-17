2147dec00e66374ba4cab5ff291a72c2ad183e98
ngotim <ngotim@users.noreply.github.com>
2019-10-17 13:10:50 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift(File.expand_path("../../", __FILE__))
-require "test-unit"
+# require "test-unit"
 require "fluent/test"
 require "fluent/test/driver/output"
 require "fluent/test/helpers"
@@ -8,4 +8,4 @@ require 'fluent/plugin/out_newrelic'
 Test::Unit::TestCase.include(Fluent::Test::Helpers)
 Test::Unit::TestCase.extend(Fluent::Test::Helpers)
 # Test::Unit.run = true if defined?(Test::Unit) && Test::Unit.respond_to?(:run=)
-Test::Unit::AutoRunner.need_auto_run = false if defined?(Test::Unit::AutoRunner)
+# Test::Unit::AutoRunner.need_auto_run = false if defined?(Test::Unit::AutoRunner)
